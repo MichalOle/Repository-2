102883146dbb28acfa46da5249fd5a7c1f168f23
Michal Ole <molesinski1994@gmail.com>
2022-12-05 15:21:08 +0100

    Inheritance exercises

---
+++ src/main/java/com/secend_Package/inheritance/newExercise/OperatingSystem.java
@@ -0,0 +1,22 @@
+package com.secend_Package.inheritance.newExercise;
+
+public class OperatingSystem {
+        private int year;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public OperatingSystem(int year){
+        this.year = year;
+    }
+    public void turnOn(){
+       System.out.println("Włączono");
+    }  public void turnOff(){
+       System.out.println("Wyłączono");
+    }
+
+}
